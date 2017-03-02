d3aa993762715e05dad543c55bda76ca3668dfe1
Peter Humbert <humbe037@umn.edu>
2017-03-02 11:57:12 -0600

    generate serialized Version 2 Project object

--- Objects/Project.cs
+++ Objects/Project.cs
@@ -4,26 +4,36 @@ namespace Objects
     [Serializable]
     public class Project
     {
-        public int int1 { get; set; } = 0;
+        public int int1 { get; set; } = 1;
         public string str1
         {
             get;
             set;
         } = "";
-
+        public int int2
+        {
+            get;
+            set;
+        } = 2;
 
         public Project()
         {
         }
 
-        public Project(int intin1)
+        public Project(int intin)
         {
-            int1 = intin1;
+            int1 = intin;
         }
         public Project(int intin1, string strin1)
         {
             int1 = intin1;
             str1 = strin1;
+        }
+        public Project(int intin1, string strin1, int intin2)
+        {
+            int1 = intin1;
+            str1 = strin1;
+            int2 = intin2;
         }
     }
 }
